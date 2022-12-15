89022c2b6c11552c7a2e2293839762cb97f768d6
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2022-12-15 15:32:46 -0300

    initial commit

---
+++ src/components/Facts.jsx
@@ -0,0 +1,12 @@
+import catEyes from '../assets/cat-eyes.png'
+
+export function Facts(){
+  return(
+    <div>
+      <div>
+        <img src={catEyes} alt="" />
+      </div>
+      <h2 style={{color: "white"}}>Fact</h2>
+    </div>
+  )
+}
